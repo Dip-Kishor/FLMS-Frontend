1b5cdf7bb8d4171144d14c5491e0fd1d333698fd
Dip-Kishor <dipkishor9910@gmail.com>
2025-03-22 15:49:37 +0545

    Fixed register and login UI

--- src/app/Auth/login/page.tsx
+++ src/app/Auth/login/page.tsx
@@ -3,7 +3,6 @@
 import React, { useState } from "react";
 import axios from "axios";
 import {port} from "@/constants/appl.constant";
-import Logo from "@/components/UI/Logo";
 import LoginButton from "@/components/UI/LoginButton";
 import Link from "next/link";
 import { usePopup } from "@/components/UI/Popup";
@@ -88,45 +87,51 @@ const Page = () => {
     {loading ? <Loading /> : <></>}
     <div className="min-h-screen flex items-center justify-center bg-[#dbe9e2] bg-cover ">
       <div className="w-[1188px] bg-[url('/image4.png')] bg-no-repeat  h-[50vh]">
-        <div className="w-[400px] mx-auto bg-white p-10 rounded-md">
-          <div className="flex items-center justify-center">
-            <Logo />
+        <div className="w-[400px] md:w-[700px] mx-auto bg-white  rounded-md flex flex-col md:flex-row">
+          <div className=" w-1/2 h-[340px] bg-cover bg-[url('/Stadium2.jpg')] text-center text-white flex flex-col justify-end p-5">
+            {/* <Logo /> */}
+            <p className="text-3xl pb-2">FLMS</p>
+            <p className="text-1xl">Login to explore more and register for up coming tournament.</p>
           </div>
-          <form onSubmit={handleSubmit}>
-            <div className="mb-4">
-              <label className="block text-sm font-medium text-gray-700">
-                Email
-              </label>
-              <input
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="mt-1 block w-full p-2 rounded-md  border-2 border-gray-300 shadow-sm focus:outline-none focus:border-[#4C6F35] sm:text-sm"
-                placeholder="user@example.com"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block text-sm font-medium text-gray-700">
-                Password
-              </label>
-              <input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="mt-1 block w-full p-2 rounded-md  border-2 border-gray-300 shadow-sm focus:outline-none focus:border-[#4C6F35] sm:text-sm"
-                placeholder="••••••••"
-                required
-              />
-            </div>
+          <div className="md:w-1/2 px-6  pt-5">
+          <p className="">Welcome Back!</p>
+            <form onSubmit={handleSubmit}>
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-700">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="mt-1 block w-full p-2 rounded-md  border-2 border-gray-300 shadow-sm focus:outline-none focus:border-[#4C6F35] sm:text-sm"
+                  placeholder="user@example.com"
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-700">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="mt-1 block w-full p-2 rounded-md  border-2 border-gray-300 shadow-sm focus:outline-none focus:border-[#4C6F35] sm:text-sm"
+                  placeholder="••••••••"
+                  required
+                />
+              </div>
 
-            <div className="w-full">
-              <LoginButton name="Login" />
+              <div className="w-full">
+                <LoginButton name="Login" />
+              </div>
+            </form>
+            <div className="link text-center py-5">
+              <p>Not have an account yet?</p>
+              <Link href="/Auth/register" className="text-center underline text-[#4C6F35]">Register here!</Link>
             </div>
-          </form>
-          <div className="link text-center py-5">
-            <p>Not have an account yet?</p>
-            <Link href="/Auth/register" className="text-center underline text-[#4C6F35]">Register here!</Link>
           </div>
         </div>
       </div>
--- src/app/Auth/register/page.tsx
+++ src/app/Auth/register/page.tsx
@@ -5,45 +5,30 @@ import { port } from "@/constants/appl.constant";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import LoginButton from "@/components/UI/LoginButton";
-import Logo from "@/components/UI/Logo";
 import { usePopup } from "@/components/UI/Popup";
-
-
+import axios from "axios";
+import Link from "next/link";
 
 const Page = () => {
     const { showPopup } = usePopup();
 
-  const [formData, setFormData] = useState({
-    username: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-  });
+    const [userName, setUserName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+ 
   const [loading, setLoading] = useState(false);
   
   const router = useRouter();
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = async () => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     setLoading(true);
-    // e.preventDefault();
-
-    if (formData.password !== formData.confirmPassword) {
-        showPopup("Password do not match.", "warning");
-
-      return;
-    }
-
+    e.preventDefault();
+    const payload ={userName,email,password}
     try {
-      const response = await fetch(`${port}/userApi/createAccount`, {
-        method: "POST",
+      const response = await axios.post(`${port}/userApi/createAccount`,payload, {
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(formData),
       });
-      const data = await response.json();
+      const data = await response.data;
 
       if (data.status == 4) {
         setLoading(false);
@@ -68,10 +53,9 @@ const Page = () => {
       {loading ? <Loading /> : <></>}
       <div className="min-h-screen flex items-center justify-center bg-[#dbe9e2] bg-cover ">
       <div className="w-[1188px] bg-[url('/image4.png')] bg-no-repeat  h-[50vh]">
-        <div className="w-[400px] mx-auto bg-white p-10 rounded-md">
-          <div className="flex items-center justify-center">
-            <Logo />
-          </div>
+        <div className="w-[400px] md:w-[700px] mx-auto bg-white  rounded-md flex flex-col md:flex-row">
+        
+          <div className="md:w-1/2 px-6  pt-5">
           <form onSubmit={handleSubmit}>
           <div className="mb-4">
               <label className="block text-sm font-medium text-gray-700">
@@ -80,61 +64,53 @@ const Page = () => {
               <input
                 type="text"
                 name="username"
-                value={formData.username}
-                onChange={handleChange}
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
                 className="mt-1 block w-full p-2 rounded-md  border-2 border-gray-300 shadow-sm focus:outline-none focus:border-[#4C6F35] sm:text-sm"
                 placeholder="user"
                 required
               />
             </div>
             <div className="mb-4">
-              <label className="block text-sm font-medium text-gray-700">
-                Email
-              </label>
-              <input
-                type="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                className="mt-1 block w-full p-2 rounded-md  border-2 border-gray-300 shadow-sm focus:outline-none focus:border-[#4C6F35] sm:text-sm"
-                placeholder="user@example.com"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block text-sm font-medium text-gray-700">
-                Password
-              </label>
-              <input
-                type="password"
-                name="password"
-                value={formData.password}
-                onChange={handleChange}
-                className="mt-1 block w-full p-2 rounded-md  border-2 border-gray-300 shadow-sm focus:outline-none focus:border-[#4C6F35] sm:text-sm"
-                placeholder="••••••••"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block text-sm font-medium text-gray-700">
-                Confirm Password
-              </label>
-              <input
-                type="password"
-                name="confirmPassword"
-                value={formData.confirmPassword}
-                onChange={handleChange}
-                className="mt-1 block w-full p-2 rounded-md  border-2 border-gray-300 shadow-sm focus:outline-none focus:border-[#4C6F35] sm:text-sm"
-                placeholder="••••••••"
-                required
-              />
-            </div>
-
+                <label className="block text-sm font-medium text-gray-700">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="mt-1 block w-full p-2 rounded-md  border-2 border-gray-300 shadow-sm focus:outline-none focus:border-[#4C6F35] sm:text-sm"
+                  placeholder="user@example.com"
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-700">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="mt-1 block w-full p-2 rounded-md  border-2 border-gray-300 shadow-sm focus:outline-none focus:border-[#4C6F35] sm:text-sm"
+                  placeholder="••••••••"
+                  required
+                />
+              </div>
             <div className="w-full">
               <LoginButton name="Sign Up" />
             </div>
           </form>
-         
+          <div className="link text-center pt-1">
+              <Link href="/Auth/login" className="text-center underline text-[#4C6F35]">Login!</Link>
+            </div>
+         </div>
+         <div className=" w-1/2 h-[340px] bg-cover bg-[url('/Stadium2.jpg')] text-center text-white flex flex-col justify-end p-5">
+            {/* <Logo /> */}
+            <p className="text-3xl pb-2">FLMS</p>
+            <p className="text-1xl">Register if you are new here.</p>
+        </div>
         </div>
       </div>
       {/* {isLoading && <div className="loading-spinner"></div>} */}
